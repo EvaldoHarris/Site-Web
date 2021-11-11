9e972910215ca4c83b1ca892a1db1b38bca58bec
EvaldoHarris <evaldo.joaoj@hotmail.com>
2021-11-10 23:00:30 -0300

    arrumando IMC e fotos

---
+++ imc.js
@@ -0,0 +1,12 @@
+function man()
+{
+    document.getElementById("woman").src="/images/woman.png"
+    document.getElementById("man").src="images/boss.png"
+    console.log("entrou")
+}
+function woman()
+{
+    document.getElementById("man").src="/images/user.png"
+    document.getElementById("woman").src="images/businesswoman.png"
+    console.log("entrou")
+}
